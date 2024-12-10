82e3629791987fd9faf5833138934cf77700ca59
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2024-12-10 09:23:52 -0500

    Creacion de DB y tablas si no existen

--- model/database_conexion.py
+++ model/database_conexion.py
@@ -1,30 +1,62 @@
-import mysql.connector  # Librería para manejar la conexión a MySQL
-from mysql.connector import Error  # Clase para manejar errores específicos de MySQL
+import mysql.connector  
+from mysql.connector import Error  
 
 
 class DatabaseConnection:
     def __init__(self):
         try:
-            # Configuración de la conexión a la base de datos
+           
             self.conn = mysql.connector.connect(
-                host='localhost',  # Dirección del servidor MySQL
-                user='root',       # Usuario de la base de datos
-                password='',       # Contraseña del usuario
-                database='calculadora'  # Nombre de la base de datos
+                host='localhost',  
+                user='root',       
+                password=''       
             )
 
-            # Verifica si la conexión fue exitosa
-            if self.conn.is_connected():
-                print()  # Mensaje de éxito
+            self.cursor = self.conn.cursor()
+
+            self.create_database('ccalculadora')
+            
+            self.conn.database = 'ccalculadora'
+
+            self.create_tables()
 
         except Error as e:
             # Manejo de errores en caso de problemas con la conexión
-            print(f"Error al conectar la base de datos: {e}")
+            print(f"Error al conectar a la base de datos: {e}")
+
+    def create_database(self, database):
+        
+        self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database};")
+
+    def create_tables(self):
+        """Crea las tablas `historial` y `operaciones` si no existen."""
+        # Crear tabla historial
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS historial (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                operacion VARCHAR(45) NOT NULL,
+                numero1 DECIMAL(10, 2) NOT NULL,
+                numero2 DECIMAL(10, 2) NOT NULL,
+                resultado DECIMAL(10, 2) NOT NULL
+            );
+        """)
+
+        # Crear tabla operaciones
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS operaciones (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                operacion VARCHAR(45) NOT NULL
+            );
+        """)
+
+        # Confirmar cambios en la base de datos
+        self.conn.commit()
 
     def Close_conection(self):
+        """Cierra la conexión a la base de datos."""
         if self.conn.is_connected():
             self.conn.close()  # Cierra la conexión
-            print()  # Mensaje de confirmación
+            print("Conexión cerrada.")
 
 
 # Bloque principal para pruebas individuales
